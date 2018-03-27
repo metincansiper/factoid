90655ccb6ffd51162fc0802db521e732cd339ae2
Dylan Fong <d2fong@uwaterloo.ca>
2018-03-27 16:58:28 -0400

    comment out redundant events, fix bug where new elements would not log for newly created elements

--- src/client/components/action-logger/index.js
+++ src/client/components/action-logger/index.js
@@ -22,16 +22,16 @@ class ActionLogger extends React.Component {
 
     const elementEvents = [
       'rename',
-      'reposition',
+      // 'reposition', // We dont really need to log reposition events
       'redescribe'
     ];
 
     const entityEvents = [
       'modify',
       'associated',
-      'associate',
+      // 'associate', // redundant event
       'unassociated',
-      'unassociate',
+      // 'unassociate',  // redundant event
       'complete',
       'uncomplete'
     ];
@@ -45,43 +45,55 @@ class ActionLogger extends React.Component {
       });
     };
 
+    let logInteractionEvts = intn => {
+      interactionEvents.forEach(evt => {
+        intn.on(evt, e => {
+          const intnName = intn.name() === '' ? `unamed` : intn.name();
+
+          pushHistory(`${evt} event for ${intnName} interaction`);
+        });
+      });
+    };
+
+    let logEntityEvts = ent => {
+      entityEvents.forEach(evt => {
+        ent.on(evt, e => {
+          const entName = ent.name() === '' ? `unamed` : ent.name();
+
+          pushHistory(`${evt} event for ${entName} entity`);
+        });
+      });
+    };
+
+    let logElementEvts = el => {
+      elementEvents.forEach(evt => {
+        el.on(evt, (e) => {
+          const elName = el.name() === '' ? `unamed ${el.type()}` : el.name();
+
+          pushHistory(`${evt} event for element ${elName}`);
+        });
+      });
+
+      if (el.isInteraction()) {
+        logInteractionEvts(el);
+      } else {
+        logEntityEvts(el);
+      }
+    };
+
     docEvents.forEach(evt => {
       this.data.document.on(evt, e => {
         const entityName = e.name() === '' ? 'unamed entity' : e.name();
 
         pushHistory(`${evt} event for ${entityName}`);
+
+        if (evt === 'add') {
+          logElementEvts(e);
+        }
       });
     });
 
-    this.data.document.elements().forEach(element => {
-      elementEvents.forEach(evt => {
-        element.on(evt, (e) => {
-          const elName = element.name() === '' ? `unamed ${element.type()}` : element.name();
-
-          pushHistory(`${evt} event for element ${elName}`);
-        });
-      });
-    });
-
-    this.data.document.entities().forEach(entity => {
-      entityEvents.forEach(evt => {
-        entity.on(evt, e => {
-          const entName = entity.name() === '' ? `unamed` : entity.name();
-
-          pushHistory(`${evt} event for ${entName} entity`);
-        });
-      });
-    });
-
-    this.data.document.interactions().forEach(interaction => {
-      interactionEvents.forEach(evt => {
-        interaction.on(evt, e => {
-          const intnName = interaction.name() === '' ? `unamed` : interaction.name();
-
-          pushHistory(`${evt} event for ${intnName} interaction`);
-        });
-      });
-    });
+    this.data.document.elements().forEach(logElementEvts);
   }
 
   render() {
