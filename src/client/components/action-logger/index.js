19467010688aa7b7d3a598477067f0216dbe7e16
Dylan Fong <d2fong@uwaterloo.ca>
2018-03-27 11:55:42 -0400

    Prototype event logger for documents, elements, entities, interactions

--- src/client/components/action-logger/index.js
+++ src/client/components/action-logger/index.js
@@ -18,14 +18,67 @@ class ActionLogger extends React.Component {
   }
 
   componentDidMount(){
-    const docEvents = ['remove', 'add'];
+    const docEvents = ['remove', 'add', 'rename', 'toggleorganism'];
+
+    const elementEvents = [
+      'rename',
+      'reposition',
+      'redescribe'
+    ];
+
+    const entityEvents = [
+      'modify',
+      'associated',
+      'associate',
+      'unassociated',
+      'unassociate',
+      'complete',
+      'uncomplete'
+    ];
+    const interactionEvents = [
+      'retype'
+    ];
+
+    let pushHistory = (histItem) => {
+      this.setState({
+        history: this.state.history.concat(histItem)
+      });
+    };
 
     docEvents.forEach(evt => {
       this.data.document.on(evt, e => {
-        const entityName = e.name() === '' ? 'unamed' : e.name();
-        console.log(evt, entityName);
-        this.setState({
-          history: this.state.history.concat(`${evt} event for ${entityName} entity`)
+        const entityName = e.name() === '' ? 'unamed entity' : e.name();
+
+        pushHistory(`${evt} event for ${entityName}`);
+      });
+    });
+
+    this.data.document.elements().forEach(element => {
+      elementEvents.forEach(evt => {
+        element.on(evt, (e) => {
+          const elName = element.name() === '' ? `unamed ${element.type()}` : element.name();
+
+          pushHistory(`${evt} event for element ${elName}`);
+        });
+      });
+    });
+
+    this.data.document.entities().forEach(entity => {
+      entityEvents.forEach(evt => {
+        entity.on(evt, e => {
+          const entName = entity.name() === '' ? `unamed` : entity.name();
+
+          pushHistory(`${evt} event for ${entName} entity`);
+        });
+      });
+    });
+
+    this.data.document.interactions().forEach(interaction => {
+      interactionEvents.forEach(evt => {
+        interaction.on(evt, e => {
+          const intnName = interaction.name() === '' ? `unamed` : interaction.name();
+
+          pushHistory(`${evt} event for ${intnName} interaction`);
         });
       });
     });
@@ -35,7 +88,6 @@ class ActionLogger extends React.Component {
     const history = this.state.history.map(entry => {
       return h('div', entry);
     });
-    console.log(history);
 
     return h('div.action-logger', history);
   }
