63aa8b154d7fe95344231e36830628bca09d70cf
fdurupinar <f.durupinar@gmail.com>
2018-04-27 11:11:48 -0700

    bug fix for new location

--- src/client/components/form-editor/location-change-form.js
+++ src/client/components/form-editor/location-change-form.js
@@ -12,25 +12,21 @@ class LocationChangeForm extends InteractionForm {
 
     let words = desc.split(' ');
 
+
     if(words.length > ind)
       return words[ind];
 
     return "";
   }
 
-  setLocation(ind, loc){
+  setOldLocation(ind, loc){
 
     const intn = this.state.interaction;
     let desc = intn.description();
 
     let words = desc.split(' ');
 
-    if(words.length > ind)
-      words[ind] = loc;
-
-    else
-      words.push(loc);
-
+    words[ind] = loc;
 
     intn.description(words.join(' '));
 
@@ -39,7 +35,22 @@ class LocationChangeForm extends InteractionForm {
   }
 
 
+  setNewLocation(ind, loc){
 
+    const intn = this.state.interaction;
+    let desc = intn.description();
+
+    let words = desc.split(' ');
+
+    words[ind-1] = "to";
+    words[ind] = loc;
+
+
+    intn.description(words.join(' '));
+
+    this.setState(this.state);
+
+  }
 
   render(){
     const intn = this.state.interaction;
@@ -55,7 +66,7 @@ class LocationChangeForm extends InteractionForm {
       h('span', [
           h('select.form-options-long', { value: intn.description(), onChange: e => this.updateInteractionType(e.target.value) }, [
               h('option', { value: 'activates translocation from ' + this.getLocation(oldLocationInd) + ' to ' + this.getLocation(newLocationInd) }, 'activates translocation'),
-              h('option', { value: 'inhibits translocation from' + this.getLocation(oldLocationInd) + ' to ' + this.getLocation(newLocationInd)  }, 'inhibits translocation'),
+              h('option', { value: 'inhibits translocation from ' + this.getLocation(oldLocationInd) + ' to ' + this.getLocation(newLocationInd)  }, 'inhibits translocation'),
           ])
       ]),
 
@@ -68,20 +79,18 @@ class LocationChangeForm extends InteractionForm {
         h('input[type="text"].' + this.state.style, {
           value: this.getLocation(oldLocationInd),
           placeholder: 'Old location',
-          onChange: e => this.setLocation(oldLocationInd, e.target.value)
+          onChange: e => this.setOldLocation(oldLocationInd, e.target.value)
         })
       ]),
 
-      // h(EntityForm, { entity: oldLocEnt, placeholder: 'Old location', style: 'form-entity-medium', document: this.state.document}),
       h('h3.form-entity-title', 'to'),
       h('div.form-interaction', [
         h('input[type="text"].' + this.state.style, {
           value: this.getLocation(newLocationInd),
           placeholder: 'Old location',
-          onChange: e => this.setLocation(newLocationInd, e.target.value)
+          onChange: e => this.setNewLocation(newLocationInd, e.target.value)
         })
       ])
-      // h(EntityForm, { entity: newLocEnt, placeholder: 'New location' , style: 'form-entity-medium', document: this.state.document}),
     ]);
   }
 }
